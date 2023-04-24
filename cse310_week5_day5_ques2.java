b687d82f8f71547fba76a5d0dee780c65f354c3b
Fayaz280 <94470550+Fayaz280@users.noreply.github.com>
2023-04-24 16:02:14 +0530

    Create cse310_week5_day5_ques2.java

---
+++ cse310_week5_day5_ques2.java
@@ -0,0 +1,29 @@
+import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        /* Enter your code here. Read input from STDIN. Print output to STDOUT. Your class should be named Solution. */
+        Scanner kl=new Scanner(System.in);
+        
+        int a=kl.nextInt();
+        int b=kl.nextInt();
+        
+       /* boolean b1=True;
+        boolean c=False;*/
+        
+        if(a>b)
+        {
+           System.out.print("true");
+        }
+        else
+        {
+            System.out.print("false");
+        }
+        
+    }
+}
