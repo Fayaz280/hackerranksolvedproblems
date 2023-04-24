1f4a786cb040bc5b0a4fa265eba35bb563854a29
Fayaz280 <94470550+Fayaz280@users.noreply.github.com>
2023-04-24 14:57:32 +0530

    Create week1_day5Q1.java

---
+++ week1_day5Q1.java
@@ -0,0 +1,19 @@
+import java.io.*;
+import java.util.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        /* Enter your code here. Read input from STDIN. Print output to STDOUT. Your class should be named Solution. */
+        Scanner kl=new Scanner(System.in);
+         int f=kl.nextInt();
+        int s=kl.nextInt();
+        int c=4*f;
+        int d=6*s;
+        s=c+d;
+        System.out.println(s);
+        
+        
+        
+    }
+}
