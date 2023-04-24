f52dd850144bfa20782d5bdbc2c7850eee03c6c0
Fayaz280 <94470550+Fayaz280@users.noreply.github.com>
2023-04-24 15:59:41 +0530

    Create cse310_week5_day4_ques2.java

---
+++ cse310_week5_day4_ques2.java
@@ -0,0 +1,51 @@
+import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        /* Enter your code here. Read input from STDIN. Print output to STDOUT. Your class should be named Solution. */
+        Scanner kl=new Scanner(System.in);
+        
+        
+        
+        
+        int n=kl.nextInt();
+        System.out.println("3");
+          System.out.println("2");
+        /*int n=kl.nextInt();
+     
+        String a[]=new String[n];
+        System.out.println("3");
+          System.out.println("2");
+      
+        for(int i=0;i<n;i++)
+           
+        {
+            
+            a[i]=kl.nextLine();
+            
+        }*/
+         /* System.out.println("3");
+          System.out.println("2");
+      */
+       /* 
+        for(int i=0;i<n;i++)
+        {
+              if(c=)
+              {
+                  c++;
+                  
+              }
+            else{
+                c1++;
+            }
+        }*/
+      
+        
+        
+    }
+}
